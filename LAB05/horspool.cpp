9566a714169c133d7eeea874180c9ae697a3abec
mayank1014 <107253117+mayank1014@users.noreply.github.com>
2023-10-26 04:31:03 +0530

    Update horspool.cpp

--- LAB05/horspool.cpp
+++ LAB05/horspool.cpp
@@ -1,48 +1,60 @@
+#include <bits/stdc++.h>
 
-#include<bits/stdc++.h>
 using namespace std;
 
+const int ALPHABET_SIZE = 256;
 
-int main(){
-    
-	//shift table
-	string str,pattern;
-	cout<<"Enter the string : "<<endl;
-	cin >> str;
-	cout<<"Enter the pattern that you want to find : "<<endl;
-	cin >> pattern;
-   
-  	int n = str.size();
-  	int m = pattern.size();
-  	int arr[26];
-  	for(int i=0;i<26;i++){
-      	arr[i] = m;
-  	}
-  	for(int i=0;i<=m-2;i++){
-    	 
-      	arr[pattern[i]-'a'] = m-i-1;
-  	}
- 	 
- 	 
-  	int j = 0;
-  	cout<<"Pattern occurred at indexes : ";
-  	while(j+m<=n){
-      	if(str[j+m-1] == pattern[m-1]){
-          	int i = m-2;
-          	while(i>=0 && pattern[i]==str[j+i]){
-              	i--;
-          	}
-          	if(i==-1){
-              	cout<<j<<" ";
-             	 
-          	}
-      	}
-      	if(str[j+m-1]=='_'||str[j+m-1]=='.'){
-          	j+=m;
-      	}
-      	else{
-      	j = j+arr[str[j+m-1]-'a'];}
-  	}
- 	 
-	return 0;
+// Function to preprocess the shift table for pattern matching
+vector<int> preprocessShiftTable(string pattern) {
+    int m = pattern.length();
+    // Initialize the shift table with a default shift value of m (pattern length)
+    vector<int> shiftTable(ALPHABET_SIZE, m);
+
+    // Fill in the shift table based on the pattern
+    for (int i = 0; i < m - 1; i++) {
+        // Calculate the shift value for the character at pattern[i]
+        shiftTable[pattern[i]] = m - 1 - i;
+    }
+
+    return shiftTable;
+}
+
+// Function to search for the pattern in the text using the Horspool algorithm
+void horsepoolSearch(string text, string pattern) {
+    int n = text.length();
+    int m = pattern.length();
+    // Preprocess the shift table
+    vector<int> shiftTable = preprocessShiftTable(pattern);
+
+    // Initialize the text index to the end of the pattern
+    int i = m - 1;
+    // Search for the pattern in the text
+    while (i < n) {
+        int k = 0;
+        // Compare characters from right to left in the pattern
+        while (k < m && pattern[m - 1 - k] == text[i - k]) {
+            k++;
+        }
+
+        // If k equals the pattern length, a match is found
+        if (k == m) {
+            cout << "Pattern found at index " << i - m + 1 << endl;
+        }
+
+        // Use the shift table to determine the next position to compare
+        i += shiftTable[text[i]];
+    }
+}
+
+int main() {
+    string text, pattern;
+    cout << "Enter the text: ";
+    cin >> text;
+    cout << "Enter the pattern to search: ";
+    cin >> pattern;
+
+    // Call the function to search for the pattern in the text
+    horsepoolSearch(text, pattern);
+
+    return 0;
 }
