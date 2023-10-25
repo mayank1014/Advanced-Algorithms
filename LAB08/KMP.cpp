172fda1c1e91517102833e890550676381e37523
mayank1014 <107253117+mayank1014@users.noreply.github.com>
2023-10-26 02:15:06 +0530

    Update KMP.cpp

--- LAB08/KMP.cpp
+++ LAB08/KMP.cpp
@@ -1,86 +1,60 @@
 #include <bits/stdc++.h>
 
-void computeLPSArray(char* pat, int M, int* lps);
-
-// Prints occurrences of txt[] in pat[]
-void KMPSearch(char* pat, char* txt)
-{
-	int M = strlen(pat);
-	int N = strlen(txt);
-
-	// create lps[] that will hold the longest prefix suffix
-	// values for pattern
-	int lps[M];
-
-	// Preprocess the pattern (calculate lps[] array)
-	computeLPSArray(pat, M, lps);
-
-	int i = 0; // index for txt[]
-	int j = 0; // index for pat[]
-	while ((N - i) >= (M - j)) {
-		if (pat[j] == txt[i]) {
-			j++;
-			i++;
-		}
-
-		if (j == M) {
-			printf("Found pattern at index %d ", i - j);
-			j = lps[j - 1];
-		}
-
-		// mismatch after j matches
-		else if (i < N && pat[j] != txt[i]) {
-			// Do not match lps[0..lps[j-1]] characters,
-			// they will match anyway
-			if (j != 0)
-				j = lps[j - 1];
-			else
-				i = i + 1;
-		}
-	}
+using namespace std;
+int m,n;
+void string_matching(string s, string p, int lps[]){
+    int q = 0;
+    cout<<"Pattern occurred at indexes : ";
+    for(int i=1; i<=n; i++){
+        while(q>0 && p[q] != s[i-1]){
+            q = lps[q];
+        }
+        
+        if(p[q] == s[i-1]){
+            q++;
+        }
+        
+        //cout<<q<<" ";
+        
+        if( q == m ){
+            cout<<i-m<<" ";
+            q = lps[q];
+            
+        }
+    }
 }
 
-// Fills lps[] for given pattern pat[0..M-1]
-void computeLPSArray(char* pat, int M, int* lps)
-{
-	// length of the previous longest prefix suffix
-	int len = 0;
 
-	lps[0] = 0; // lps[0] is always 0
+int main(){
 
-	// the loop calculates lps[i] for i = 1 to M-1
-	int i = 1;
-	while (i < M) {
-		if (pat[i] == pat[len]) {
-			len++;
-			lps[i] = len;
-			i++;
-		}
-		else // (pat[i] != pat[len])
-		{
-			// This is tricky. Consider the example.
-			// AAACAAAA and i = 7. The idea is similar
-			// to search step.
-			if (len != 0) {
-				len = lps[len - 1];
+    string s;
+    cout<<"Enter the string : "<<endl;
+    cin>>s;
+     n = s.size();
+    
+    string pattern ;
+    cout<<"Enter pattern : "<<endl;
+    cin>>pattern;
+     m = pattern.size();
+    
+    
+    //preprocessing
+    int p_table[m+1];
+    p_table[1] = 0;
+    int k=0;
+    for(int q=2;q<=m;q++ ){
+        while(k>0 && pattern[k]!= pattern[q-1])
+           k = p_table[k];
+           
+        if(pattern[k] == pattern[q-1])
+           k++;
+           
+        p_table[q] = k;
+    }
+    
+    
+    string_matching(s, pattern, p_table);
+	
 
-				// Also, note that we do not increment
-				// i here
-			}
-			else // if (len == 0)
-			{
-				lps[i] = 0;
-				i++;
-			}
-		}
-	}
-}
-
-// Driver code
-int main()
-{
-	char txt[] = "ABABDABACDABABCABAB";
-	char pat[] = "ABABCABAB";
-	KMPSearch(pat, txt);
 	return 0;
 }
