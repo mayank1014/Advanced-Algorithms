73285bf79ab316d1861722815d4f079700e6ca52
mayank1014 <107253117+mayank1014@users.noreply.github.com>
2023-10-26 00:00:27 +0530

    Update isPrime.cpp

--- LAB02/isPrime.cpp
+++ LAB02/isPrime.cpp
@@ -1,71 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-vector<bool> decimalToBinary(long long int num){
-    vector<bool> ans;
-    while(0<num){
-        ans.push_back(1&num);
-        num >>= 1;
-    }
-    return ans;
-}
-
-long long int power(long long int base,long long int exponent,long long int mod){
-    if(base>mod)
-        base %= mod;
-        
-    long long ans = 1;
+unsigned long long int gcd(unsigned long long int a, unsigned long long int b){
     
-    auto bin = decimalToBinary(exponent);
+	while(b!=0){
+    	unsigned long long int r =a%b;
+    	a=b;
+    	b=r;
+	}
+    	return a;
+}
+
+unsigned long long int power(unsigned long long int a,unsigned long long int x, unsigned long long int n){
+	unsigned long long int res = 1;
+	a = a%n;
+	while(x>0){
+    	if(x%2 ==1)
+      	res= (res*a)%n;
+     	 
+    	x=x/2;
+   	 
+    	a=(a*a)%n;
+	}
+	return res;
+}
+
+float find_liars(unsigned long long int n){
+	int count = 0;
     
-    int len = bin.size();
-    for(int i=len-1;0<=i;i--){
-        ans = ((ans%mod)*(ans%mod))%mod;
-        if(true==bin[i]){
-            ans = ((ans%mod)*(base%mod))%mod;
-        }
-    }
-    return ans;
+	for(int a=2;a<=n-2;a++){
+    	if(power(a,n-1,n)==1)
+      	count++;
+	}
+    
+	return count/float(n-3);
+}
+bool is_Prime(unsigned long long int n){
+    
+	int k = 100;
+	if(n<=1 || n==4)
+  	return false;
+ 	 
+ 	if( n<=3 )
+   	return true;
+ 	 
+	while(k>0){
+   	 
+    	srand(time(0));
+    	unsigned long long int a = 2 + rand()%(n-4);
+   	 
+    	if(gcd(n,a) != 1)
+       	return false;
+      	 
+    	if(power(a,n-1,n) != 1)
+       	return false;
+      	 
+    	k--;
+	}
+    
+    
+	return true;
 }
 
-long long int gcd(long long int num1,long long int num2){
-    if(0==num2) return num1;
-    return gcd(num2,num1%num2);
-}
-
-long long int iterativeGcd(long long int num1,long long int num2){
-    while(0!=num2){
-        long long int rem = num1%num2;
-        num1 = num2;
-        num2 = rem;
-    }
-    return num1;
-}
-
-// O(k*log(number))
-bool isPrime(long long int number,long long int k,long long int mod){
-    srand(time(NULL));
-    while(0<k){
-        long long int a = 2+(rand()%(number-3));
-        
-        if(1!=iterativeGcd(a,number))
-            return false;
-        long long temp = power(a,mod-1,mod);
-        if(1!=temp)
-            return false;
-        k--;
-    }
-    return true;
-}
-
-int main() {
-    long long int num,k,mod;
-    mod = 1000000007;
-    cin>>num>>k>>mod;
-    if(isPrime(num,k,mod)){
-        cout<<"Given Number is Probably Prime!!"<<endl;
-    }else{
-        cout<<"Given Number is Not Prime!!"<<endl;
-    }
+int main()
+{
+   unsigned long long int n;
+   cout<<"Enter a number : "<<endl;
+   cin>>n;
+   
+   bool ans = is_Prime(n);
+   
+   if(ans == true)
+   cout<<n<<" is a prime number"<<endl;
+   
+   else
+   cout<<n<<" is not a prime number"<<endl;
+   
+   float probability = find_liars(n);
+   
+	cout<<"Probability of f_liars is : "<<probability<<endl;
+   
+	return 0;
 }
