7c1adb2f93217b22c8e19520f68768e8e5d62bdc
mayank1014 <107253117+mayank1014@users.noreply.github.com>
2023-10-26 04:39:39 +0530

    Update naive_string_matching.cpp

--- LAB05/naive_string_matching.cpp
+++ LAB05/naive_string_matching.cpp
@@ -1,35 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> naiveApproach(string &text,string &pattern){
+// Function for the naive string matching approach
+vector<int> naiveApproach(string &text, string &pattern) {
     int n = text.size();
     int m = pattern.size();
-    if(n<m) return {};
-    vector<int> ans;
-    for(int t=0;t<=n-m;t++){
+    if (n < m)
+        return {};  // Return an empty vector if the text is shorter than the pattern.
+    vector<int> ans;  // Initialize a vector to store the indices of pattern occurrences.
+
+    // Iterate through the text from the beginning.
+    for (int t = 0; t <= n - m; t++) {
         bool is_matched = true;
-        for(int p=0;p<m;p++){
-            if(text[t+p]!=pattern[p]){
+
+        // Check if the pattern matches the text starting at index t.
+        for (int p = 0; p < m; p++) {
+            if (text[t + p] != pattern[p]) {
                 is_matched = false;
-                break;
+                break;  // Break if a character mismatch is found.
             }
         }
-        if(is_matched){
+
+        // If the pattern is fully matched, add the index t to the result vector.
+        if (is_matched) {
             ans.push_back(t);
         }
     }
-    return ans;
+    return ans;  // Return the vector of pattern occurrences.
 }
 
-int main(){
-   string str,pattern;
-	cout<<"Enter the string : "<<endl;
-	cin >> str;
-	cout<<"Enter the pattern that you want to find : "<<endl;
-	cin >> pattern;
-    vector<int> indexs = naiveApproach(str,pattern);
-    for(int i : indexs){
-        cout<<"pattern match at : "<<i<<"\n";
+int main() {
+    string str, pattern;
+    cout << "Enter the string: ";
+    cin >> str;
+    cout << "Enter the pattern that you want to find: ";
+    cin >> pattern;
+
+    // Call the naiveApproach function to find pattern occurrences in the text.
+    vector<int> indices = naiveApproach(str, pattern);
+
+    // Display the indices where the pattern is found in the text.
+    for (int i : indices) {
+        cout << "Pattern match at index: " << i << "\n";
     }
     return 0;
 }
