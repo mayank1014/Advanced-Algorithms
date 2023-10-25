216f4077b9352232115ed2608ddb3cb339695e36
mayank1014 <107253117+mayank1014@users.noreply.github.com>
2023-10-26 00:32:22 +0530

    Update horspool.cpp

--- LAB05/horspool.cpp
+++ LAB05/horspool.cpp
@@ -1,46 +1,48 @@
-#include <bits/stdc++.h>
+
+#include<bits/stdc++.h>
 using namespace std;
 
-int findPattern(string &text,string &pattern){
-    int n = text.size();
-    int m = pattern.size();
-    // preprocessing start
-    map<char,int> shiftTable;
-    unordered_set<char> chInPattern;
-    for(int i=0;pattern.size()-1>i;i++){
-        shiftTable[pattern[i]] = m-1-i;
-        chInPattern.insert(pattern[i]);
-    }
-    chInPattern.insert(pattern.back());
-    
-    // preprocessing end
 
-    int j=0;
-    while(j+m<n){
-        if(pattern[m-1]==text[j+m-1]){
-            int i = m-2;
-            while(0<=i && pattern[i]==text[j+i])
-                i--;
-            if(-1==i)
-                return j;
-        }
-        cout<<j<<" ";
-        if(0==shiftTable[text[j+m-1]])
-            j += m;
-        else
-            j += shiftTable[text[j+m-1]];
-    }
-    return -1;
-}
 int main(){
-    string text,pattern;
-    getline(cin,text);
-    getline(cin,pattern);
-    int index = findPattern(text,pattern);
-    if(0>index){
-        cout<<"Pattern not found!!"<<endl;
-    }else{
-        cout<<"Pattern found at "<<index<<"th index"<<endl;
-    }
-    return 0;
+    
+	//shift table
+	string str,pattern;
+	cout<<"Enter the string : "<<endl;
+	cin >> str;
+	cout<<"Enter the pattern that you want to find : "<<endl;
+	cin >> pattern;
+   
+  	int n = str.size();
+  	int m = pattern.size();
+  	int arr[26];
+  	for(int i=0;i<26;i++){
+      	arr[i] = m;
+  	}
+  	for(int i=0;i<=m-2;i++){
+    	 
+      	arr[pattern[i]-'a'] = m-i-1;
+  	}
+ 	 
+ 	 
+  	int j = 0;
+  	cout<<"Pattern occurred at indexes : ";
+  	while(j+m<=n){
+      	if(str[j+m-1] == pattern[m-1]){
+          	int i = m-2;
+          	while(i>=0 && pattern[i]==str[j+i]){
+              	i--;
+          	}
+          	if(i==-1){
+              	cout<<j<<" ";
+             	 
+          	}
+      	}
+      	if(str[j+m-1]=='_'||str[j+m-1]=='.'){
+          	j+=m;
+      	}
+      	else{
+      	j = j+arr[str[j+m-1]-'a'];}
+  	}
+ 	 
+	return 0;
 }
