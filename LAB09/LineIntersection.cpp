b0234696ed0c0f1c34db5fb82136c33de9f834ee
mayank1014 <107253117+mayank1014@users.noreply.github.com>
2023-10-26 05:50:18 +0530

    Update LineIntersection.cpp

--- LAB09/LineIntersection.cpp
+++ LAB09/LineIntersection.cpp
@@ -1,66 +1,65 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-struct point{
-	double x;
-	double y;
+
+// Define a structure to represent a 2D point
+struct point {
+    double x;
+    double y;
 };
 
-//Check the direction of p1p3 with respect to p1p2
-double direction(struct point p1, struct point p2, struct point p3){
-	double a = p3.x - p1.x ;
-	double b = p3.y - p1.y ;
-	double c = p2.x - p1.x ;
-	double d = p2.y - p1.y ;
+// Function to calculate the direction of three points
+// It calculates the cross product to determine direction
+double direction(struct point p1, struct point p2, struct point p3) {
+    double a = p3.x - p1.x;
+    double b = p3.y - p1.y;
+    double c = p2.x - p1.x;
+    double d = p2.y - p1.y;
     
-	return (a*d)-(b*c) ;
+    return (a * d) - (b * c);
 }
 
-//check whether p3 is on segment p1p2 or not
-bool onSegment (struct point p1,struct point p2, struct point p3){
-	if(min(p1.x,p2.x)<= p3.x && p3.x<=max(p1.x,p2.x) && min(p1.y,p2.y)<=p3.y && p3.y<=max(p1.y,p2.y))
-    	return true;
-    
-	else
-   	return false;
+// Function to check if a point is on a line segment
+bool onSegment(struct point p1, struct point p2, struct point p3) {
+    if (min(p1.x, p2.x) <= p3.x && p3.x <= max(p1.x, p2.x) && min(p1.y, p2.y) <= p3.y && p3.y <= max(p1.y, p2.y))
+        return true;
+    else
+        return false;
 }
 
-bool intersect(struct point p1, struct point p2, struct point p3, struct point p4){
-	double d1 = direction(p1, p2, p3);
-	double d2 = direction(p1, p2, p4);
-	double d3 = direction(p3, p4, p1);
-	double d4 = direction(p3, p4, p2);
+// Function to check if two line segments intersect
+bool intersect(struct point p1, struct point p2, struct point p3, struct point p4) {
+    double d1 = direction(p1, p2, p3);
+    double d2 = direction(p1, p2, p4);
+    double d3 = direction(p3, p4, p1);
+    double d4 = direction(p3, p4, p2);
     
-	if((d1*d2)<0 && (d3*d4)<0)
-   	return true;
-  	 
-	else if(d1==0 && onSegment(p1, p2, p3) == true)
-   	return true;
+    // Check for intersection based on relative directions
+    // if the product of d1 and d2 is negative, it means P3 and P4 are on different sides of the line
+    if ((d1 * d2) < 0 && (d3 * d4) < 0)
+        return true;
     
-	else if(d2==0 && onSegment(p1, p2, p4) == true)
-   	return true;
+    // Check if any of the points lie on the other segment
+    if (d1 == 0 && onSegment(p1, p2, p3))
+        return true;
+    if (d2 == 0 && onSegment(p1, p2, p4))
+        return true;
+    if (d3 == 0 && onSegment(p3, p4, p1))
+        return true;
+    if (d4 == 0 && onSegment(p3, p4, p2))
+        return true;
     
-	else if(d3==0 && onSegment(p3, p4, p1) == true)
-   	return true;
-    
-	else if(d4==0 && onSegment(p3, p4, p2) == true)
-   	return true;
-    
-	else
-  	return false;
+    return false;
 }
 
-int main()
-{
-	struct point p1,p2,p3,p4;
-	cout<<"Enter p1 p2 p3 and p4 : "<<endl;
-	cin>>p1.x>>p1.y>>p2.x>>p2.y>>p3.x>>p3.y>>p4.x>>p4.y;
+int main() {
+    struct point p1, p2, p3, p4;
+    cout << "Enter p1 p2 p3 and p4 coordinates: " << endl;
+    cin >> p1.x >> p1.y >> p2.x >> p2.y >> p3.x >> p3.y >> p4.x >> p4.y;
     
-	if(intersect(p1, p2, p3, p4))
-	cout<<"P1P2 and P3P4 intersect.";
-    
-	else
-	cout<<"P1P2 and P3P4 do not intersect";
+    if (intersect(p1, p2, p3, p4))
+        cout << "P1P2 and P3P4 intersect." << endl;
+    else
+        cout << "P1P2 and P3P4 do not intersect." << endl;
 
-	return 0;
+    return 0;
 }
